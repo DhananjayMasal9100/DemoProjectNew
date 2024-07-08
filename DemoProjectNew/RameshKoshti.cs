d64240e8a52dc30b7517de78ebaeb0aadfdf9c74
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-08 09:55:39 +0530

    program using get set keyword

--- DemoProjectNew/RameshKoshti.cs
+++ DemoProjectNew/RameshKoshti.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoProjectNew
 {
+    public class Person
+    {
+        string pname;
+        int page;
+        public string Name
+        {
+            get {  return pname; }  
+            set { pname = value; }
+        }
+
+        public int Age
+        {
+            get; set;
+        }
+        public void paddress()
+        {
+            Console.WriteLine("paddress");
+        }
+    }
+
     class RameshKoshti
     {
         static void Main()
         {
-            Console.WriteLine("Hello...!");
-            Console.WriteLine("Ramesh...!");
+            Person person = new Person();
+            person.Name = "Ramesh";
+            Console.WriteLine(person.Name);
+            person.Age = 29;
+            Console.WriteLine(person.Age);
+            person.paddress();
         }
     }
 }
