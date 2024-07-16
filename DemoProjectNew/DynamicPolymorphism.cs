01166f16f97c8b593a266dc9626cd01879f10281
Dhananjay Masal <dhananjay9096@gmail.com>
2024-07-16 08:24:20 +0530

    PolyMorphism Example

---
+++ DemoProjectNew/DynamicPolymorphism.cs
@@ -0,0 +1,65 @@
+﻿using DemoProjectNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    //Base Class
+    //Method name same parameer also same but different class
+    class Shape
+    {
+
+        //virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+
+        }
+    }
+
+    //Derved class 
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+
+            Console.WriteLine("drawwing circle");
+
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+        }
+    }
+
+
+
+    internal class DynamicPolymorphism
+    {
+        static void Main()
+        {
+            //creating object
+
+
+            Shape shape1 = new Shape();
+            Shape shape2 = new Circle();
+            Shape shape3 = new Rectangle();
+
+            //calling method from each object
+            shape1.Draw();
+            shape2.Draw();
+            shape3.Draw();
+
+
+        }
+    }
+}
+
---
+++ DemoProjectNew/StaticPolymorphism.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    //Method overloading/static poly
+    //   Method name same but  parameter different in same class
+    class MathOpertions
+    {
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+    internal class StaticPolymorphism
+    {
+        static void Main()
+        {
+            MathOpertions math = new MathOpertions();
+            //call overloading method
+
+            int sum1 = math.Add(2, 3);
+            double sum2 = math.Add(6, 5);
+
+
+            Console.WriteLine("Sum  of Integers:"+ sum1);
+            Console.WriteLine("Sum of Doubles:"+ sum2);
+
+
+        }
+
+    }
+}
