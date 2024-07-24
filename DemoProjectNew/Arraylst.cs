a0dee4063d8770ee980185fb23afee079c412c2a
Dhananjay Masal <dhananjay9096@gmail.com>
2024-07-24 08:36:13 +0530

    Example of non generic collection

---
+++ DemoProjectNew/Arraylst.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DemoProjectNew
+{
+    internal class Arraylst
+    {
+        static void Main()
+        {
+            //creting An Array List
+            ArrayList lst = new ArrayList();  
+            //add element in to the Arraylist
+
+            lst.Add(1);
+            lst.Add("Hello");
+            lst.Add(false);
+
+            //Access the elements/items 
+            Console.WriteLine("Arraylist Elements:");
+            foreach(var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Removing element from arraylist
+
+            lst.Remove(false);
+
+
+            //Access the elements after removal
+
+            Console.WriteLine("Arraylist Elements after removal:");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ DemoProjectNew/NongenQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    internal class NongenQueue
+    {
+        static void  Main()
+        {
+            Queue qu = new Queue();
+            //FIFO - Fist in First Out
+            //Enque is basically used to add element in to the queue
+
+            qu.Enqueue("Sachin");
+            qu.Enqueue(123654);
+            qu.Enqueue(true);
+
+            // Dequeue (remove) - to remove the element from the queue
+
+            object firstelement = qu.Dequeue();
+            object SecondElement = qu.Dequeue();
+            object ThirdElement = qu.Dequeue();
+
+            //Display the items dequeueed
+
+            Console.WriteLine("Dequeued Elements");
+            Console.WriteLine(firstelement);
+            Console.WriteLine(SecondElement);
+            Console.WriteLine(ThirdElement);
+
+        }
+    }
+}
