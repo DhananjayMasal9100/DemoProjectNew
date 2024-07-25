0a89300c11e187ce980918bc21cacd712b073d57
Dhananjay Masal <dhananjay9096@gmail.com>
2024-07-25 08:44:52 +0530

    Stack and Dictionary example

---
+++ DemoProjectNew/Dixct.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    internal class Dixct
+    {
+
+        static void Main()
+        {
+            //initialize dictionary
+            Dictionary<string,int> agedict = new Dictionary<string,int>();
+
+            //add Key-Value pains to the dictinoary
+
+            agedict.Add("Rohit", 27);
+            agedict.Add("Sangram", 24);
+            agedict.Add("Sachin", 25);
+            
+            //print all the elements in the dictoinry
+            Console.WriteLine("All elements in the Dictionary");
+            foreach(KeyValuePair<string,int> KVP in agedict)
+            {
+                Console.WriteLine($"{KVP.Key}: {KVP.Value}");
+            }
+
+            //print specific Items from the dictinorary
+            Console.WriteLine("Age of 'Rohit': "+ agedict["Rohit"] );
+            Console.WriteLine("Age of 'Sangram': "+ agedict["Sangram"]);
+
+            //Modify the values form dictionary
+
+            agedict["Rohit"] = 29;
+            agedict["Sachin"] = 22;
+
+
+            //Print all modified values from dictionary
+            Console.WriteLine("All elements in the Dictionary");
+            foreach (KeyValuePair<string, int> KVP in agedict)
+            {
+                Console.WriteLine($"{KVP.Key}: {KVP.Value}");
+            }
+
+
+            //remove value
+            agedict.Remove("Sachin");
+
+
+
+            if(agedict.ContainsKey("Rohit"))
+            {
+                Console.WriteLine("Exist in the dictionary");
+            }
+
+
+            //to clear the disctionary
+
+            agedict.Clear();
+
+
+            Console.WriteLine(agedict.Count);
+
+        }
+    }
+}
---
+++ DemoProjectNew/NonGenStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    internal class NonGenStack
+    {
+        //LIFO- Last In First Out
+        static void Main()
+        {
+            //create a non- generic stack 
+            Stack st = new Stack();
+
+            st.Push("Top");
+            st.Push(42);
+            st.Push(false);
+
+            //Remove( POP) the element from stack
+            object topelement = st.Pop();
+            object secondelement = st.Pop();
+            object thridelement = st.Pop();
+
+            //Dispay the poped elements
+
+            Console.WriteLine("Poped elements");
+            Console.WriteLine(topelement);
+            Console.WriteLine(secondelement);
+            Console.WriteLine(thridelement);
+
+            //check   if the stack is empty or it has a value
+
+            Console.WriteLine("Is sthe stack empty?"+(st.Count ==0));
+
+
+        }
+
+
+    }
+}
