21ff68232c169c83c8ecaee3accb1cfc1ffd6658
Dhananjay Masal <dhananjay9096@gmail.com>
2024-08-02 08:14:26 +0530

    sealed class and statc class

---
+++ DemoProjectNew/SealedClassxample.cs
@@ -0,0 +1,41 @@
+﻿using DemoProjectNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    public  class BaseClass
+    {
+        public virtual int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public sealed  override int add(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+    internal class SealedClassxample
+    {
+        static void Main()
+
+        {
+            int a = 10;
+            int b = 20;
+            DerivedClass d = new DerivedClass();
+            BaseClass B = new BaseClass();
+            d.add(a, b);
+            B.add(a, b);
+
+        }
+    }
+}
---
+++ DemoProjectNew/StaticClassExample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+
+    //sstatic class
+    static class Product
+    {
+        //static Members     
+        public static int ProductId;
+        public static string ProductName;
+        public static int ProductPrice;
+
+        //static Constructor 
+        static Product()
+        {
+            ProductId = 1100;
+            ProductName = "Test";
+            ProductPrice = 1000;
+        }
+        //static methods
+
+        public static void GetProdctDetails()
+        {
+            Console.WriteLine("Product id :{0}",ProductId);
+            Console.WriteLine("Product Name :{0}",ProductName);
+            Console.WriteLine("Product Price : {0} ",ProductPrice);
+
+        }
+       public static void GetDiscount()
+        {
+            int D_amount = ProductPrice /10;
+            Console.WriteLine("Your Discount amount is :{0}", D_amount);
+            Console.WriteLine("Total Amount of Product: {0}" ,(ProductPrice -D_amount));
+
+
+        }
+    }
+
+   
+    internal class StaticClassExample
+    {
+        static void Main()
+        {
+            //Product p = new Product();
+
+            Product.GetProdctDetails();
+
+            Product.GetDiscount();
+            Console.ReadLine();
+
+            
+
+        }
+
+    }
+}
